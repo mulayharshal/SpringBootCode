fb545a630c5f4f0b405e81e4b876e865a2c3e8f5
Harshal Mulay <harshalmulay1039@gmail.com>
2025-08-04 19:16:25 +0530

    in spring boot ecom project code 4Aug

--- Project Ecommerce/ecom-proj/src/main/java/com/Telusko/ecom_proj/controller/ProductController.java
+++ Project Ecommerce/ecom-proj/src/main/java/com/Telusko/ecom_proj/controller/ProductController.java
@@ -1,14 +1,37 @@
 package com.Telusko.ecom_proj.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.Telusko.ecom_proj.model.Product;
+import com.Telusko.ecom_proj.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
-    @RequestMapping
-    public String greet(){
-        return "Hello World";
+
+    @Autowired
+    private ProductService service;
+
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>( service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+        }
     }
 
 }
